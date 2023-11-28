96a6da7346d37ba84d50473a39028d8d42aadb77
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-28 11:09:27 +0100

    Fixed DI order in CLI

--- SejmCalendar.CLI/Program.cs
+++ SejmCalendar.CLI/Program.cs
@@ -7,11 +7,11 @@ var builder = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
         var config = context.Configuration;
+        services.AddSingleton<SejmDataAccess>();
         services.AddHttpClient<SejmDataAccess>(client =>
         {
             client.BaseAddress = new Uri(config.GetValue<string>("API:Sejm:BaseUrl") ?? "");
         });
-        services.AddSingleton<SejmDataAccess>();
     });
 
 var host =  builder.Build();
