3fbc8d835f2ddab16feddd14195ebd96c7dac80d
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-28 19:13:51 +0100

    Change BirthdayService in DI to scoped

---
+++ SejmCalendar.Blazor/Program.cs
@@ -0,0 +1,36 @@
+using SejmCalendar.Blazor.Components;
+using SejmCalendar.Library;
+using SejmCalendar.Library.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient<ISejmDataAccess, SejmDataAccess>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("API:Sejm:BaseUrl") ?? "");
+});
+builder.Services.AddScoped<IBirthdayService, BirthdayService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
