4fe1b75850a8c5f7a81606edad663e6fd55da95f
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-28 19:11:46 +0100

    Add MonthyCalendar component

---
+++ SejmCalendar.Blazor/Models/DayModel.cs
@@ -0,0 +1,9 @@
+﻿using SejmCalendar.Library.DataAccess;
+
+namespace SejmCalendar.Blazor.Models;
+
+public class DayModel
+{
+    public DateTime Date { get; set; }
+    public List<SejmMPRecord> SejmMPs { get; set; } = new();
+}
