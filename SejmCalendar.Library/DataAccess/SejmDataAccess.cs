bf876ef9f4d73f6a78f7678abe5108399a1cf0f9
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-28 11:47:38 +0100

    Remove unnecesary usings, folder

--- SejmCalendar.Library/DataAccess/SejmDataAccess.cs
+++ SejmCalendar.Library/DataAccess/SejmDataAccess.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using System.Net.Http;
-using System.Net.Http.Json;
+﻿using System.Net.Http.Json;
 
 namespace SejmCalendar.Library.DataAccess;
 public class SejmDataAccess : ISejmDataAccess
