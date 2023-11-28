2be4d84c11c4f18f4970f40514a9ebfafc95a08a
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-28 11:50:14 +0100

    Add BirthdayService class

---
+++ SejmCalendar.Library/BirthdayService.cs
@@ -0,0 +1,12 @@
+﻿using SejmCalendar.Library.DataAccess;
+
+namespace SejmCalendar.Library;
+public class BirthdayService
+{
+    private readonly ISejmDataAccess _dataAccess;
+
+    public BirthdayService(ISejmDataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+}
