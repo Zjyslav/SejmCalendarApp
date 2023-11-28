b6431971cae93c5fcbd9db3319b03911c914bb0a
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-28 09:30:21 +0100

    Add solution, .Library and .CLI projects

---
+++ SejmCalendar.Library/DataAccess/SejmDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+
+namespace SejmCalendar.Library.DataAccess;
+public class SejmDataAccess
+{
+    private readonly HttpClient _httpClient;
+
+    public SejmDataAccess(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+}
