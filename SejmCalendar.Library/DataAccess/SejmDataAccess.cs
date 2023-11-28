042b6b65affbc95568f894423bb31e8e052eb2b9
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-28 11:28:46 +0100

    Add GetAllMPs method and record added to SejmDataAccess

--- SejmCalendar.Library/DataAccess/SejmDataAccess.cs
+++ SejmCalendar.Library/DataAccess/SejmDataAccess.cs
@@ -17,6 +17,12 @@ public class SejmDataAccess
         List<SejmTermRecord>? output = await _httpClient.GetFromJsonAsync<List<SejmTermRecord>>("sejm/term");
         return output ?? new();
     }
+
+    public async Task<List<SejmMPRecord>> GetAllMPsByTermId(int termId)
+    {
+        List<SejmMPRecord>? output = await _httpClient.GetFromJsonAsync<List<SejmMPRecord>>($"sejm/term{termId}/MP");
+        return output ?? new();
+    }
 }
 
 #region records
@@ -30,4 +36,23 @@ public record PrintsRecord(int Count,
                            DateTime LastChanged,
                            string Link);
 
+public record SejmMPRecord(int Id,
+                     string FirstLastName,
+                     string LastFirstName,
+                     string FirstName,
+                     string SecondName,
+                     string LastName,
+                     string Email,
+                     bool Active,
+                     string InactiveCause,
+                     string WaiverDesc,
+                     int DistrictNum,
+                     string DisctrictName,
+                     string Voivodeship,
+                     string Club,
+                     DateTime BirthDate,
+                     string BirthLocation,
+                     string Profession,
+                     string EducationLevel,
+                     int NumberOfVotes);
 #endregion
