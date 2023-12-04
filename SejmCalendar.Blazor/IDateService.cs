5beeeb629f1480fbf489a416ceb04c936ea4bab2
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-12-04 15:58:25 +0100

    Add DateService and interface

---
+++ SejmCalendar.Blazor/IDateService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SejmCalendar.Blazor;
+
+public interface IDateService
+{
+    DateTime Date { get; }
+
+    event Action? OnDateChange;
+
+    void AddDays(int days);
+    void SetDate(DateTime date);
+}
